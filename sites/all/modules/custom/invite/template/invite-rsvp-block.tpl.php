58d13d50cd74d74939a9f5b648db9e9d72e852e2
Joel Rothschild <joel.rothschild@jacksonriver.com>
2013-02-24 13:30:51 -0500

    Added Invite custom feature in progress.

---
+++ sites/all/modules/custom/invite/template/invite-rsvp-block.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Variables:
+ *   $different_event - TRUE if the session's invite isn't for the event page being viewed
+ *   $rsvp - 'planned', 'notified', 'responded', 'attending', 'regrets'
+ *   $invite_name - full name on the session's invite
+ *   $form - the RSVP form for an invite that isn't already either attending or regrets
+ */
+?>
+<?php if ($different_event) : ?>
+  <h3>To RSVP</h3>
+  <p>If you received an invitation email <em>for this event</em>, use the link in that email to view the RSVP form.</p>
+
+<?php elseif ($form) : ?>
+  <h3>Please RSVP!</h3>
+  <?php print render($form); ?>
+  
+<?php else : ?>
+  <h3>You're <?php if ($rsvp == 'regrets') print 'Not '?>Attending.</h3>
+  <p>Thank you for your RSVP.</p>
+
+<?php endif; ?>
