c92d58bf3fc55c9ead8c6d948bf6368ba855d0c4
Joel Rothschild <joel.rothschild@jacksonriver.com>
2013-06-10 08:48:50 -0400

    Bugfix to RSVP form javascript: If invitee selects "Not Attending," the "How Many Attending?" field needs to be auto-populated with a zero so it can be hidden without invalidating the form.

--- sites/all/modules/custom/invite/js/rsvp-form.js
+++ sites/all/modules/custom/invite/js/rsvp-form.js
@@ -5,15 +5,19 @@ Drupal.behaviors.invite_node_form = {
     function toggleFields(e) {
       if ($('#invite-rsvp-form #edit-field-invite-rsvp-und-0-value-attending').get(0).checked) {
         // toggle on the attending-only fields
+        if ($('#edit-field-invite-attendee-count-und-0-value').attr('value') == 0) {
+          $('#edit-field-invite-attendee-count-und-0-value').attr('value', '');
+        }
         $('#invite-rsvp-form .form-item-field-invite-attendee-count-und-0-value').show();
         $('#invite-rsvp-form .form-item-field-invite-contributions-und-0-value').show();
       } else {
         // toggle off the attending-only fields
+        $('#edit-field-invite-attendee-count-und-0-value').attr('value', '0');
         $('#invite-rsvp-form .form-item-field-invite-attendee-count-und-0-value').hide();
         $('#invite-rsvp-form .form-item-field-invite-contributions-und-0-value').hide();
       }
     }
-    // toggle when the bulk loading is switched on or off
+    // toggle when an RSVP status is selected
     $('#invite-rsvp-form .form-item-field-invite-rsvp-und-0-value input[type=radio]').change(toggleFields);
   }
 };
